10c7d57e52717a51bf8c1bddd456b6141dcb39a0
Pessoa <ppessoa@asu.edu>
2024-11-04 15:05:27 -0700

    bash

---
+++ bash.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+python make_data.py
+
+#echo "filename,method,N,KL_MAP,KL_reconstruct" > "report.csv"
+#echo "filename,method,N,KL_MAP,KL_reconstruct" > "report_prod.csv"
+
+
+n_values=(100 316 1000 3160 10000)
+methods=("SUM_Bayes.py" "SUM_NPBayes.py" "SUM_Normflow.py" "PROD_Bayes.py" "PROD_NPBayes.py" "PROD_Normflow.py")
+methods=("PROD_Normflow.py")
+
+
+for method in "${methods[@]}"
+do
+    for i in {3..9}
+    do
+        for n in "${n_values[@]}"
+        do
+            echo "Running $method with n = $n, i = $i"
+            python "$method" "$n" "$i"
+        done
+    done
+done
