d776dcfe188da2b12d2e95d100e67badc4fe3be1
Pessoa <ppessoa@asu.edu>
2024-11-26 12:16:33 -0700

    NFdeconvolute officialized

---
+++ paper_examples/nf_class.py
@@ -0,0 +1,9 @@
+# This file is a "forwarding" module. 
+# It allows the nf_class module to remain usable in this folder, even though the actual library (NFdeconvolute.py) is in a different folder.
+
+import sys  
+import os   
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, parent_dir)
+from NFdeconvolute import *
